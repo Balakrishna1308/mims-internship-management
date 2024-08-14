fcba22bfbbb53dd9739e7c812fe5d1f025228742
Bala <balakrishna.malipeddi@gmail.com>
2024-08-14 17:17:50 +0530

    Initial project setup using Spring Initializer

---
+++ src/main/java/com/mfa/mims/MimsApplication.java
@@ -0,0 +1,13 @@
+package com.mfa.mims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimsApplication.class, args);
+	}
+
+}
