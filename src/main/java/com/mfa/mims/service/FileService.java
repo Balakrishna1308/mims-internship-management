5bfeddf9f370ca4dc317fc3497e7ff7b8616a23d
Bala <balakrishna.malipeddi@gmail.com>
2024-09-24 20:42:49 +0530

    commits saved.

--- src/main/java/com/mfa/mims/service/FileService.java
+++ src/main/java/com/mfa/mims/service/FileService.java
@@ -1,14 +1,3 @@
-//package com.mfa.mims.service;
-//
-//import java.io.File;
-//import java.util.concurrent.CompletableFuture;
-//
-//public interface FileService {
-//    CompletableFuture<Boolean> uploadFile(File file);
-//}
-
-
-
 package com.mfa.mims.service;
 
 import org.springframework.web.multipart.MultipartFile;
